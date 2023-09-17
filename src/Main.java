01f52ff2cfdff7ea18a40f07ae3b7308c270c07c
pobanzkj <pobanzkj@mail.uc.edu>
2023-09-17 18:53:00 -0400

    MonthlyCCBalance

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-public class Main {
-    static double balance = 5000;
-    static double interestrate = .17;
-    static double monthlypayment1 = balance * interestrate;
-static double x = monthlypayment1 * interestrate;
-    static double monthlypayment2 = monthlypayment1 + monthlypayment1 + x;
-
-    public static void main(String[] args) {
+public class Main { public static void main(String[] args) {
+    double balance = 5000;
+    double interestrate = .17;
+    double monthlypayment1 = balance * interestrate;
+    double x = monthlypayment1 * interestrate;
+    double monthlypayment2 = monthlypayment1 + monthlypayment1 + x;
         System.out.println("Your interest due after one month is $" + monthlypayment1);
         System.out.println("Your interest due after two months is $" + monthlypayment2);
     }
